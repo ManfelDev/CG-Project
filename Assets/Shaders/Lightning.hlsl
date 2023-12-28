83ec2e6fa4bb293da1940fe7df095ef38b7ac2a4
Rafael José <ra.david@hotmail.com>
2023-12-28 21:37:59 +0000

    Add shadows to Lambert lighting model

--- Assets/Shaders/Lightning.hlsl
+++ Assets/Shaders/Lightning.hlsl
@@ -1,23 +1,74 @@
-void MainLightInformation_float(out float3 Direction, out float3 Color)
+#if defined(SHADERGRAPH_PREVIEW)
+#else
+    #pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+#endif
+
+void MainLightInformation_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAttenuation, out float ShadowAttenuation)
 {
     #if SHADERGRAPH_PREVIEW
         Direction = float3(0.5, 0.5, 0);
         Color = 1;
+        DistanceAttenuation = 1;
+        ShadowAttenuation = 1;
     #else
-        Light mainLight = GetMainLight();
-        Direction = mainLight.direction;
-        Color = mainLight.color;
+        #if SHADOWS_SCREEN
+            float4 clipPos = TransformWorldToHClip(WorldPos);
+            float4 shadowCoord = ComputeScreenPos(clipPos);
+        #else
+            float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAttenuation = mainLight.distanceAttenuation;
+
+    #if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
+        ShadowAttenuation = 1.0;
+    #endif
+
+    #if SHADOWS_SCREEN
+        ShadowAttenuation = SampleScreenSpaceShadowmap(shadowCoord);
+    #else
+        ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+        float shadowStrength = GetMainLightShadowStrength();
+        ShadowAttenuation = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false);
+    #endif
+
     #endif
 }
 
-void MainLightInformation_half(out half3 Direction, out half3 Color)
+void MainLightInformation_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAttenuation, out half ShadowAttenuation)
 {
     #if SHADERGRAPH_PREVIEW
         Direction = half3(0.5, 0.5, 0);
         Color = 1;
+        DistanceAttenuation = 1;
+        ShadowAttenuation = 1;
     #else
-        Light mainLight = GetMainLight();
-        Direction = mainLight.direction;
-        Color = mainLight.color;
+        #if SHADOWS_SCREEN
+            half4 clipPos = TransformWorldToHClip(WorldPos);
+            half4 shadowCoord = ComputeScreenPos(clipPos);
+        #else
+            half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAttenuation = mainLight.distanceAttenuation;
+
+    #if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
+        ShadowAttenuation = 1.0h;
+    #endif
+
+    #if SHADOWS_SCREEN
+        ShadowAttenuation = SampleScreenSpaceShadowmap(shadowCoord);
+    #else
+        ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+        half shadowStrength = GetMainLightShadowStrength();
+        ShadowAttenuation = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false);
+    #endif
+
     #endif
 }
