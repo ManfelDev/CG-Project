c078565efecfa23ab78e4c9990a2c3c183f595a1
Rafael José <ra.david@hotmail.com>
2023-12-29 00:03:12 +0000

    Add specular

--- Assets/Shaders/Lighting.hlsl
+++ Assets/Shaders/Lighting.hlsl
@@ -72,3 +72,27 @@ void MainLightInformation_half(float3 WorldPos, out half3 Direction, out half3 C
 
     #endif
 }
+
+void DirectSpecular_float (float3 Specular, float Smoothness, float3 Direction, float3 Color, float3 WorldNormal, float3 WorldView, out float3 Out)
+{
+    #if SHADERGRAPH_PREVIEW
+        Out = 0;
+    #else
+        Smoothness = exp2(10 * Smoothness + 1);
+        WorldNormal = normalize(WorldNormal);
+        WorldView= SafeNormalize(WorldView);
+        Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, float4(Specular, 0), Smoothness);
+    #endif
+}
+
+void DirectSpecular_half (half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
+{
+    #if SHADERGRAPH_PREVIEW
+        Out = 0;
+    #else
+        Smoothness = exp2(10 * Smoothness + 1);
+        WorldNormal = normalize(WorldNormal);
+        WorldView= SafeNormalize(WorldView);
+        Out = LightingSpecular(Color, Direction, WorldNormal, WorldView, half4(Specular, 0), Smoothness);
+    #endif
+}
